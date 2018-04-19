2136fa614554368c7b5d749df82662249865977a
FLAVIO HENRIQUE ROCHA FERREIRA <flavio.hrferreira@senacsp.edu.br>
2018-04-19 11:11:09 -0300

    Declaração de Variáveis

--- OperacoesCondicionais/Program.cs
+++ OperacoesCondicionais/Program.cs
@@ -6,7 +6,15 @@ namespace OperacoesCondicionais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Comentário in-line
+            /*
+            Comentário in-block(em bloco) 
+            */
+            //Declaração da variável operação. Ela será do tipo inteira.
+            int operacao;
+            //Declaração das variáveis n1 e n2. Elas serão do tipo double.
+            double n1, n2;
+            
         }
     }
 }
